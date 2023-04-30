36009fd879a34112ba4533a1b99b512404b71418
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-04-30 12:50:53 -0300

    test(user): returns the correct hero title

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
     expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid
   end
-  it "returns the correct hero title"
+  it "returns the correct hero title" do
+    user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
+    expect(user.title).to eq('wizard Chronos #1')
+  end
 end
