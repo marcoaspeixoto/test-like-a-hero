3de73e99c89d96e7d99054a0cf6ddd7e157117a1
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-02 17:37:39 -0300

    test(weapon): creat a random weapon

--- spec/factories/weapons.rb
+++ spec/factories/weapons.rb
@@ -1,9 +1,6 @@
 FactoryBot.define do
   factory :weapon do
-    name { "MyString" }
-    description { "MyString" }
-    power_base { 1 }
-    power_step { 1 }
-    level { 1 }
+    name { FFaker::Name.name }
+    description { FFaker::Lorem.phrase }
   end
 end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Weapon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creat a random weapon' do
+    weapon = build(:weapon)
+    expect(weapon).to be_valid
+  end
+
 end
