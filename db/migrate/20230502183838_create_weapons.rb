c2dfcb912f36fd4613ebfdbd32568d32b013eb5c
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-02 16:09:28 -0300

    feat(weapon): migration

--- db/migrate/20230502183838_create_weapons.rb
+++ db/migrate/20230502183838_create_weapons.rb
@@ -3,9 +3,9 @@ class CreateWeapons < ActiveRecord::Migration[6.1]
     create_table :weapons do |t|
       t.string :name
       t.string :description
-      t.integer :power_base
-      t.integer :power_step
-      t.integer :level
+      t.integer :power_base, default: 3000
+      t.integer :power_step, default: 100
+      t.integer :level, default: 1
 
       t.timestamps
     end
