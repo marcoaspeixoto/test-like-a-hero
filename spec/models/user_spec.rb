7bbcbffd811a970f08cb9a483b406902eaf106c0
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-02 15:06:45 -0300

    refactor(test user): factory_bot_rails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
+require 'spec/factories/user'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
-
+    user = build(:user, level: FFaker::Random.rand(100..9999))
     expect(user).to_not be_valid
   end
   it "returns the correct hero title" do
@@ -14,7 +11,7 @@ RSpec.describe User, type: :model do
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(1..99)
 
-    user = User.create(nickname: nickname, kind: kind, level: level)
+    user = create(:user, nickname: nickname, kind: kind, level: level)
     expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
