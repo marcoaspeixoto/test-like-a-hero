922f87055e5aaad768f8bf03534c8954bef777f3
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-02 16:18:08 -0300

    feat(weapon): validates

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,2 +1,6 @@
 class Weapon < ApplicationRecord
+  validates :power_step, numericality: {equal_to: 100}
+  validates :power_base, numericality: {greater_than_or_equal_to: 3000}
+  validates :level, numericality: {greater_than: 0, less_than_or_equal_to: 99}
+
 end
