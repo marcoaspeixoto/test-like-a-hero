c589e85d795cf544c61391c8eebbe9b848a0a344
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-04 16:00:59 -0300

    test(user): returns success status

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
+require 'spec/factories/user'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
     it "returns success status" do
-
+      get users_path
+      expect(response).to have_http_status(200)
     end
 
     it "the user's title is present" do
@@ -14,10 +16,12 @@ RSpec.describe "Users", type: :request do
   describe "POST /users" do
     context "when it has valid parameters" do
       it "creates the user with correct attributes"
+
     end
 
     context "when it has no valid parameters" do
       it "does not create user"
+
     end
   end
 end
