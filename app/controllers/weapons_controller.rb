9828a430f5c41a56063fdcd1c39e3a7850c80009
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-06 17:54:46 -0300

    feat(weapon): create

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,8 +1,11 @@
 class WeaponsController < ApplicationController
   def index
+    @weapons = Weapon.all
   end
 
   def create
+    @weapon = Weapon.create(user_params)
+    redirect_to weapons_path
   end
 
   def delete
