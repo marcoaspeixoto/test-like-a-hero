2818d0af7af13bf76667da9596f78e023e85fe5a
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-05 21:56:34 -0300

    test(user): creates the user with correct attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redicrect_to users_path
+    redirect_to users_path
   end
 
   private
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,7 +19,11 @@ RSpec.describe "Users", type: :request do
 
   describe "POST /users" do
     context "when it has valid parameters" do
-      it "creates the user with correct attributes"
+      it "creates the user with correct attributes" do
+        user_attributes = FactoryBot.attributes_for(:user)
+        post users_path, params: { user: user_attributes}
+        expect(User.last).to have_attributes(user_attributes)
+      end
 
     end
 
