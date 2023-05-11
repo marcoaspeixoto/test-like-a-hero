60ee7144709dd673ba60777a3eface1b2dd9519e
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-11 16:04:02 -0300

    test(weapon): displays the link to each weapon

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -31,23 +31,12 @@ RSpec.describe "Weapons", type: :request do
         expect(response.body).to include(weapon.title)
       end
     end
-  end
 
-  describe "POST /users" do
-    context "when it has valid parameters" do
-      it "creates the user with correct attributes" do
-        user_attributes = FactoryBot.attributes_for(:user)
-        post users_path, params: { user: user_attributes}
-        expect(User.last).to have_attributes(user_attributes)
-      end
-
-    end
-
-    context "when it has no valid parameters" do
-      it "does not create user" do
-        expect{
-          post users_path, params: { user: {kind: '', name: '', level: ''}}
-        }.to_not change(User, :count)
+    it "displays the link to each weapon" do
+      weapons = create_list(:weapon, 3)
+      get weapons_path
+      weapons.each do |weapon|
+        expect(response.body).to include("<a href=\"#{weapon_path(weapon)}\">#{weapon.name}</a>")
       end
     end
   end
