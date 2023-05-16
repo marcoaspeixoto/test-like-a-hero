6370a94dad4a9cef0fdb634dab842c4045203193
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-16 14:44:00 -0300

    feat(enemies): auxiliary methods

--- app/controllers/enemies_controller.rb
+++ app/controllers/enemies_controller.rb
@@ -1,7 +1,21 @@
 class EnemiesController < ApplicationController
+  before_action :set_enemy
+
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def enemy_params
+    params.permit(:name, :power_base, :power_step, :level, :kind)
+  end
+
+  def set_enemy
+    @enemy = Enemy.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { message: e.message }, status: :not_found
   end
 end
