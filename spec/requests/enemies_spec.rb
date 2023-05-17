37cb53f0417623e0300154ff403d08608938e6aa
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-05-17 19:44:35 -0300

    test(enemies): returns status code 200

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -4,7 +4,12 @@ require 'spec/factories/enemies'
 RSpec.describe "Enemies", type: :request do
   describe "GET /enemies" do
     context "when the enemy exists" do
-      it "returns status code 200"
+      it "returns status code 200" do
+        enemy = create(:enemy)
+        enemy_attributes = attributes_for(:enemy)
+        put "/enemies/#{enemy.id}", params: enemy_attributes
+        expect(response).to have_http_status(200)
+      end
       it "updates the record"
       it "returns the enemy updated"
     end
